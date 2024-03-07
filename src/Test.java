b0b0a2d13b145653b96bd2d5e8a8e2c7631a0aac
PongpisutFlook <145266199+PongpisutFlook@users.noreply.github.com>
2024-03-07 22:04:31 +0700

    Add prototype
    Add prototype retirement and short-saving system

---
+++ src/Test.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author LAPTOP_PT
+ */
+public class Test {
+    public static void main(String[] args) {
+        char i = '2';
+        char j = '3';
+        System.out.println((int)(i+j));
+        
+    }
+}
