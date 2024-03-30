7b8c59ba27f5445878ce76a0102884123696f92e
Worachai Pikunkaew <aom.dev27@gmail.com>
2024-03-30 23:16:01 +0700

    Ads now are 200*400

--- ConsultAdsSystem/src/AdsImageIcon.java
+++ ConsultAdsSystem/src/AdsImageIcon.java
@@ -1,13 +1,27 @@
 import javax.swing.*;
+import java.io.*;
+import java.awt.*;
+import javax.imageio.ImageIO;
+import java.awt.image.*;
 
 public class AdsImageIcon extends ImageIcon{
     private String name;
     private String hyperLink;
     
     public AdsImageIcon(String name, String hyperLink, byte[] imageBytes){
-        super(imageBytes);
+        super();
+        this.setImage(imgByteToImage(imageBytes));
         this.name = name;
         this.hyperLink = hyperLink;
+    }
+    
+    private Image imgByteToImage(byte[] imageBytes){
+        try{
+            BufferedImage img = ImageIO.read(new ByteArrayInputStream(imageBytes));
+            return img.getScaledInstance(200, 400, Image.SCALE_SMOOTH);
+        }catch(IOException e){
+            return null;
+        }
     }
     
     public String getFileName(){
